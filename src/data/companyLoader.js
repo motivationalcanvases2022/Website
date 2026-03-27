b4a561179718641771fd04e41ba5a3d9e6042958
Kim <kimskogh04@gmail.com>
2026-03-27 16:51:05 +0100

    Add kmcgroup site + updates

--- src/data/companyLoader.js
+++ src/data/companyLoader.js
@@ -2,17 +2,19 @@ import dentist from "./companies/dentist.json";
 import gym from "./companies/gym.json";
 import restaurant from "./companies/restaurant.json";
 import electrician from "./companies/electrician.json";
+import kmcgroup from "./companies/kmcgroup.json";
 
 const companies = {
   dentist,
   gym,
   restaurant,
-  electrician
+  electrician,
+  kmcgroup
 };
 
 export function getCompanyData() {
   const params = new URLSearchParams(window.location.search);
-  const companyName = params.get("company") || "dentist";
+  const companyName = params.get("company") || "kmcgroup";
 
-  return companies[companyName] || dentist;
+  return companies[companyName] || kmcgroup;
 }
